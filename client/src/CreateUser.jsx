956f11326b71cfd46d69906077d081d2476c7acd
JOYRU <joycseru@gmail.com>
2024-05-18 00:37:15 +0600

    file add for create update and show user info

--- client/src/CreateUser.jsx
+++ client/src/CreateUser.jsx
@@ -2,8 +2,30 @@ import React from 'react'
 
 export default function CreateUser() {
   return (
-    <div>
-       Create User
-    </div>
+    <article>
+     <form>
+      <div class="form-group container center_div">
+        <label for="inputName" class="col-sm-2 col-form-label">Name</label>
+        <div class="col-sm-5 mb-2">
+          <input type="name" class="form-control" id="name" placeholder="Name"/>
+        </div>
+      </div>
+      <div class="form-group container center_div">
+        <label for="inputEmail" class="col-sm-2 col-form-label">Email</label>
+        <div class="col-sm-5">
+          <input type="email" class="form-control" id="email" placeholder="Email"/>
+        </div>
+      </div>
+      <div class="form-group container center_div">
+        <label for="inputAge" class="col-sm-2 col-form-label">Age</label>
+        <div class="col-sm-5">
+          <input type="text" class="form-control" id="age" placeholder="Age"/>
+        </div>
+      </div>
+      <div>
+        <button className='btn btn-success container center_div col-sm-2  ' >Submit</button>
+      </div>
+    </form>
+  </article>
   )
 }
--- client/src/Users.jsx
+++ client/src/Users.jsx
@@ -1,4 +1,5 @@
 import React, { useState } from 'react'
+import {Link} from "react-router-dom" ;
 
 export default function Users() {
     const [users,setUsers] = useState([{
@@ -7,6 +8,7 @@ export default function Users() {
   return (
     <div className="d-flex vh-100 bg-primary justify-content-center align-items-center">
       <div className='w-50 bg-white rounded p-3'>
+      <Link to='/create' className="btn btn-success">Add+</Link>
       <table class="table">
   <thead>
     <tr>
