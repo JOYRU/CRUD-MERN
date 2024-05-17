308c548afcba87ced31315f536850ccd0dd2ce90
JOYRU <joycseru@gmail.com>
2024-05-18 00:02:16 +0600

    crud operation with react node mongodb

---
+++ client/README.md
@@ -0,0 +1,6 @@
+# Crud Operation with react node mongodb
+
+#npm init vite
+#npm bootstrap axios react-router-dom 
+#npm install 
+#npm run dev
---
+++ client/src/Users.jsx
@@ -0,0 +1,39 @@
+import React, { useState } from 'react'
+
+export default function Users() {
+    const [users,setUsers] = useState([{
+        Name : "joy" , Email:"jou@gmail.com" , Age: 20
+    }])
+  return (
+    <div className="d-flex vh-100 bg-primary justify-content-center align-items-center">
+      <div className='w-50 bg-white rounded p-3'>
+      <table class="table">
+  <thead>
+    <tr>
+      <th scope="col">Name</th>
+      <th scope="col">Email</th>
+      <th scope="col">Age</th>
+      <th scope="col">Action</th>
+    </tr>
+  </thead>
+  <tbody>
+
+    
+      {
+        users.map((user)=>{
+          return  <tr>
+                <td>{user.Name}</td>
+                <td>{user.Email}</td>
+                <td>{user.Age}</td>
+                <td><button>Edit</button><button>Delete</button></td>
+            </tr>
+        })
+      }
+     
+
+  </tbody>
+</table>
+      </div>
+    </div>
+  )
+}
